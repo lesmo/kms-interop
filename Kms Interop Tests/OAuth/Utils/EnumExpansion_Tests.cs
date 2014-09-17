6437ed9011f562e7266f156ad70cb769f233bbe3
Sinuhé Coronel <lesmo@lesmo.com.mx>
2014-09-17 16:06:55 -0500

    Añadido proyecto de UnitTesting.

---
+++ Kms Interop Tests/OAuth/Utils/EnumExpansion_Tests.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Kms.Interop.OAuth.Utils;
+using Xunit;
+namespace Kms.Interop.OAuth.Utils.Tests {
+    public class EnumExpansion_Tests {
+        [Fact()]
+        public void CamelCaseToUnderlineString_Test() {
+            Assert.True(false, "not implemented yet");
+        }
+    }
+}
